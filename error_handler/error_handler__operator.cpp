6e91c9fd993b6ad7093577f0b3f1e2b2535fc878
violador <dogmacentral@hotmail.com>
2013-10-03 11:29:46 -0300

    update before Alberto's fork

---
+++ error_handler/error_handler__operator.cpp
@@ -0,0 +1,8 @@
+#include "error_handler.hpp"
+//
+//
+//
+void error_handler::operator =(const int &error)
+{
+    evaluate_behavior(const int &error);
+}
--- test__catalyst.cpp
+++ test__catalyst.cpp
@@ -7,115 +7,53 @@
 #include "array.hpp"
 #include "file_system.hpp"
 #include "log_file_handler.hpp"
+#include "global_log.hpp"
+#include "timer.hpp"
 //
 //
 //
 int main(int argc, char* argv[])
 {
-    settings config;
-    //log_file_handler file(config);
-    molecular_system pyr("data/H.xyz", config);
-    pyr.molecular_system::electronic_energy();
 /*
-    file.output << "testando do main" << std::endl;
-    file.output.width(50);
-    file.output.fill(' ');
-    file.output << "Positions";
+    settings a;
+    timer my_clock(a);
+    boost::timer::cpu_timer a;
+    boost::timer::cpu_timer b;
+    a.boost::timer::cpu_timer::start();
+    my_clock.timer::start();
+    for (long i = 0; i < 100000000; ++i)
+    {
+        std::sqrt(123.456L); // burn some time
+        std::cout << "estou no loop!" << std::endl;
+        //std::cout << boost::timer::format(a.boost::timer::cpu_timer::elapsed(), (short int) 10,"%w") << std::endl;
+    }
+    my_clock.timer::stop();
+    std::cout << "wall = " << my_clock.timer::get(1) << std::endl;
+    std::cout << "sys  = " << my_clock.timer::get(2) << std::endl;
+    std::cout << "user = " << my_clock.timer::get(3) << std::endl;
+    std::cout << "cpu  = " << my_clock.timer::get(4) << std::endl;
 */
 /*
-    array A(2, 2);
-    array B(2, 2);
-    array C(2, 2);
-    double a = 2.0;
-    A.array::set(1, 1, 1.0); 
-    A.array::set(1, 2, 1.0); 
-    A.array::set(2, 1, 1.0); 
-    A.array::set(2, 2, 1.0);
-    B.array::set(1, 1, 2.0); 
-    B.array::set(1, 2, 2.0); 
-    B.array::set(2, 1, 2.0); 
-    B.array::set(2, 2, 2.0);
-    std::cout << "SOMA" << std::endl;
-    C = A * a;
-    std::cout << "C11 = " << C.array::get(1, 1) << std::endl; 
-    std::cout << "C12 = " << C.array::get(1, 2) << std::endl; 
-    std::cout << "C21 = " << C.array::get(2, 1) << std::endl; 
-    std::cout << "C22 = " << C.array::get(2, 2) << std::endl; 
-    std::cout << "SUB" << std::endl;
-    C = A - B;
-    std::cout << "C11 = " << C.array::get(1, 1) << std::endl; 
-    std::cout << "C12 = " << C.array::get(1, 2) << std::endl; 
-    std::cout << "C21 = " << C.array::get(2, 1) << std::endl; 
-    std::cout << "C22 = " << C.array::get(2, 2) << std::endl; 
+    a.boost::timer::cpu_timer::stop();
+    std::cout << "time a = " << boost::timer::format(a.boost::timer::cpu_timer::elapsed(), (short int) 10,"%w") << std::endl;
+//
+    b.boost::timer::cpu_timer::start();
+    b.boost::timer::cpu_timer::stop();
+    std::cout << "time b = " << boost::timer::format(b.boost::timer::cpu_timer::elapsed(), (short int) 10,"%w") << std::endl;
 */
+//
 /*
-    std::cout << "A11 = " << A.array::get(1, 1) << std::endl; 
-    std::cout << "A12 = " << A.array::get(1, 2) << std::endl; 
-    std::cout << "A21 = " << A.array::get(2, 1) << std::endl; 
-    std::cout << "A22 = " << A.array::get(2, 2) << std::endl; 
-    std::cout << "A is transposed = " << A.array::check_if(IS_TRANSPOSED) << std::endl;
-    std::cout << " " << std::endl;
-    std::cout << "B11 = " << B.array::get(1, 1) << std::endl; 
-    std::cout << "B12 = " << B.array::get(1, 2) << std::endl; 
-    std::cout << "B21 = " << B.array::get(2, 1) << std::endl; 
-    std::cout << "B22 = " << B.array::get(2, 2) << std::endl; 
-*/
-/*
-    std::cout << "A is transposed = " << A.array::check_if(IS_TRANSPOSED) << std::endl;
-    std::cout << "B is transposed = " << B.array::check_if(IS_TRANSPOSED) << std::endl;
-    std::cout << " " << std::endl;
-    std::cout << "C11 = " << C.array::get(1, 1) << std::endl; 
-    std::cout << "C12 = " << C.array::get(1, 2) << std::endl; 
-    std::cout << "C21 = " << C.array::get(2, 1) << std::endl; 
-    std::cout << "C22 = " << C.array::get(2, 2) << std::endl; 
-    std::cout << "C is transposed = " << C.array::check_if(IS_TRANSPOSED) << std::endl;
-*/
-/*
-    std::cout << "A11 = " << A.array::get(1, 1) << std::endl; 
-    std::cout << "A12 = " << A.array::get(1, 2) << std::endl; 
-    std::cout << "A21 = " << A.array::get(2, 1) << std::endl; 
-    std::cout << "A22 = " << A.array::get(2, 2) << std::endl; 
-    std::cout << "A is transposed = " << A.array::check_if(IS_TRANSPOSED) << std::endl;
-    std::cout << " " << std::endl;
-*/
-/*
-    C = A*B;
-    D = C*A;
-    std::cout << "A11 = " << A.array::get(1, 1) << std::endl; 
-    std::cout << "A12 = " << A.array::get(1, 2) << std::endl; 
-    std::cout << "A21 = " << A.array::get(2, 1) << std::endl; 
-    std::cout << "A22 = " << A.array::get(2, 2) << std::endl; 
-    std::cout << "B11 = " << B.array::get(1, 1) << std::endl; 
-    std::cout << "B12 = " << B.array::get(1, 2) << std::endl; 
-    std::cout << "B21 = " << B.array::get(2, 1) << std::endl; 
-    std::cout << "B22 = " << B.array::get(2, 2) << std::endl; 
-    std::cout << "C11 = " << C.array::get(1, 1) << std::endl; 
-    std::cout << "C12 = " << C.array::get(1, 2) << std::endl; 
-    std::cout << "C21 = " << C.array::get(2, 1) << std::endl; 
-    std::cout << "C22 = " << C.array::get(2, 2) << std::endl; 
-    std::cout << "D11 = " << D.array::get(1, 1) << std::endl; 
-    std::cout << "D12 = " << D.array::get(1, 2) << std::endl; 
-    std::cout << "D21 = " << D.array::get(2, 1) << std::endl; 
-    std::cout << "D22 = " << D.array::get(2, 2) << std::endl; 
-    C = A*B;
-    D = B*A;
-    std::cout << "DEPOIS" << std::endl;
-    std::cout << "A11 = " << A.array::get(1, 1) << std::endl; 
-    std::cout << "A12 = " << A.array::get(1, 2) << std::endl; 
-    std::cout << "A21 = " << A.array::get(2, 1) << std::endl; 
-    std::cout << "A22 = " << A.array::get(2, 2) << std::endl; 
-    std::cout << "B11 = " << B.array::get(1, 1) << std::endl; 
-    std::cout << "B12 = " << B.array::get(1, 2) << std::endl; 
-    std::cout << "B21 = " << B.array::get(2, 1) << std::endl; 
-    std::cout << "B22 = " << B.array::get(2, 2) << std::endl; 
-    std::cout << "C11 = " << C.array::get(1, 1) << std::endl; 
-    std::cout << "C12 = " << C.array::get(1, 2) << std::endl; 
-    std::cout << "C21 = " << C.array::get(2, 1) << std::endl; 
-    std::cout << "C22 = " << C.array::get(2, 2) << std::endl; 
-    std::cout << "D11 = " << D.array::get(1, 1) << std::endl; 
-    std::cout << "D12 = " << D.array::get(1, 2) << std::endl; 
-    std::cout << "D21 = " << D.array::get(2, 1) << std::endl; 
-    std::cout << "D22 = " << D.array::get(2, 2) << std::endl; 
+    tools a;
+    std::map<int, double> my_map;
+    std::map<int, double>::iterator i = my_map.begin();
+    my_map.insert(i, std::pair<int, double>(1, a.generate_double_type(1)));
+    my_map.insert(i, std::pair<int, double>(2, a.generate_double_type(2)));
+    std::cout << my_map[1] << std::endl;
+    std::cout << my_map[2] << std::endl;
+    my_map.insert(i, std::pair<int, double>(3, a.generate_double_type(3)));
+    std::cout << my_map[3] << std::endl;
+    std::cout << my_map[4] << std::endl;
+    std::cout << my_map[0] << std::endl;
 */
     return 0;
 }
