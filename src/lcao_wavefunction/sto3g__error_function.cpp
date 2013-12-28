23a48085e5fc9388dda25407c4c1b58a4c339563
Humberto Jr. <dogmacentral@hotmail.com>
2013-12-28 02:22:27 -0200

    Delete sto3g__error_function.cpp

--- src/lcao_wavefunction/sto3g__error_function.cpp
+++
@@ -1,8 +0,0 @@
-#include "sto3g.hpp"
-//
-//
-//
-double lcao_wavefunction::sto3g::error_function(const double &value)
-{
-    return ((value <= 0.000001)? 1.0 - (value/3.0) : 0.5*sqrt(M_PI/value)*gsl_sf_erf(sqrt(value)));
-}
